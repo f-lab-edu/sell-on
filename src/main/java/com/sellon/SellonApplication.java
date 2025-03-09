fff039f6dce16bb52326bee908fc5dd449386184
gumgu <bienlee@naver.com>
2025-03-10 00:10:58 +0900

    initial commit

---
+++ src/main/java/com/sellon/SellonApplication.java
@@ -0,0 +1,14 @@
+package com.sellon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellonApplication {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		SpringApplication.run(SellonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sellon/SellonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
