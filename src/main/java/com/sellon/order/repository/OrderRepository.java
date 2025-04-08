c9c5293623e3a6174276232bd5c8076936c74312
bienlee <bienlee388@gmail.com>
2025-04-08 18:54:20 +0900

    feat(Payment): 정산(Payment) Service에 결제수단 추가
    관련이슈: #30

--- src/main/java/com/sellon/order/repository/OrderRepository.java
+++ src/main/java/com/sellon/order/repository/OrderRepository.java
@@ -1,9 +1,11 @@
 package com.sellon.order.repository;
 
 import com.sellon.order.entity.Order;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    Optional<Order> findByOrderNumber(String orderNumber);
 }
